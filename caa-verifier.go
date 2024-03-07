6868ba6d784b5fd06606ccc30a81897066b01bd5
zunda <zundan@gmail.com>
2024-03-07 22:22:44 +0000

    Don't panic

--- caa-verifier.go
+++ caa-verifier.go
@@ -11,14 +11,15 @@ import (
 func main() {
 	if len(os.Args) < 2{
 		fmt.Fprintf(os.Stderr, "usage: %v <domain>\nverifies CAA recrods\n", os.Args[0])
-		os.Exit(1)
+		os.Exit(0)
 	}
 	domain := os.Args[1]
 	issuers := []string{"letsencrypt.org"}
 
 	err := util.ValidateCAA(domain, issuers, false, util.RecursiveNameservers);
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	fmt.Printf("Non-wildcard certificate for %v can be issued by %v.\n", domain, strings.Join(issuers, ","))
 }
