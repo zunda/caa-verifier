505a59ff316909b52bc76d03e2a4f1c8efad6f86
zunda <zundan@gmail.com>
2024-03-08 00:06:20 +0000

    Add -v option to modify log level

--- caa-verifier.go
+++ caa-verifier.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/cert-manager/cert-manager/pkg/issuer/acme/dns/util"
+	"k8s.io/klog/v2"
 )
 
+func usage() string {
+	return fmt.Sprintf("usage: %v <domain>\nverifies CAA recrods.\n", os.Args[0])
+}
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprint(flag.CommandLine.Output(), usage())
+		fmt.Fprint(flag.CommandLine.Output(), "\nCommand line options:\n")
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: %v <domain>\nverifies CAA recrods\n", os.Args[0])
+	klog.InitFlags(nil)
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Fprint(os.Stderr, usage())
+		fmt.Fprint(os.Stderr, "Supply -help option for command line options\n")
 		os.Exit(0)
 	}
-	domain := os.Args[1]
+	domain := flag.Arg(0)
 	issuers := []string{"letsencrypt.org"}
 
 	err := util.ValidateCAA(domain, issuers, false, util.RecursiveNameservers)
